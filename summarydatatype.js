2c44ce3836d200c13b8d2dfefa7cd7e8b46ce50b
avadhutsawant3317 <Sawantavadhut76@gmail.com>
2025-07-27 01:22:40 +0530

    commit

---
+++ summarydatatype.js
@@ -0,0 +1,31 @@
+// datatypes 
+// primitive
+
+// String,number,boolean,null,undefined,symbol,bigint
+
+// constscore = 100;
+// const valscore = 100;
+
+// const IsloggedIn = true;
+// const outsideTemp = 30;
+// const useremail = "undefined";
+
+
+// const bigintValue = 1234567890123456789012345678901234567890n;
+
+// non-primitive
+// Object,Array,Function   
+
+// const heros = ["Thor", "Ironman", "Hulk"];
+// const user = {
+//   name: "John",     
+//     age: 30,    
+//     isLoggedIn: true,
+// }
+
+const myfunction = function() {
+  console.log("Hello, World!");
+}
+
+console.log(typeof myfunction); // "function"
+
